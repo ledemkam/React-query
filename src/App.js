2a357a74960843fd794ac235d8337bcd3e59325c
ledemkam <ledemkam@gmail.com>
2022-06-10 01:06:40 +0200

    query

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import { useQuery, useMutation } from "react-query";
+import "./App.css";
+
+const fetchUsers = async () => {
+  const res = await fetch("https://reqres.in/api/users");
+  return res.json();
+};
 
 function App() {
+  const { isLoading, data, isError } = useQuery("users", fetchUsers);
+  // console.log(data);
+
+  const addUser = async (user) => {
+    const res = await fetch("https://reqres.in/api/users", {
+      method: "POST",
+      body: JSON.stringify({
+        first_name: user.first_name,
+        last_name: user.first_name,
+        email: user.email,
+      }),
+      headers: {
+        "Content-Type": "application/json; chartset=UTF-8",
+      },
+    });
+    return res.json();
+  };
+
+  const mutation = useMutation(addUser, {
+    onError: (error, variable, context) => {
+      console.log("Error");
+    },
+    onSuccess: (data, variable, context) => {
+      console.log(data);
+    },
+  });
+
+  if (isLoading) {
+    return (
+      <div className="App">
+        <h1>Laden..</h1>
+      </div>
+    );
+  }
+  if (isError) {
+    return (
+      <div className="App">
+        <h1>Erreur..</h1>
+      </div>
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {mutation.isLoading ? (
+        "Adding todo..."
+      ) : (
+        <>
+          {mutation.isError ? (
+            <div>An error occurred: {mutation.error.message}</div>
+          ) : null}
+
+          {mutation.isSuccess ? <div>user added!</div> : null}
+
+          <button
+            onClick={() => {
+              mutation.mutate({
+                first_name: "Rico",
+                last_name: "Kells",
+                email: "ricokells@aol.com",
+              });
+            }}
+          >
+            Create user
+          </button>
+        </>
+      )}
     </div>
   );
 }
